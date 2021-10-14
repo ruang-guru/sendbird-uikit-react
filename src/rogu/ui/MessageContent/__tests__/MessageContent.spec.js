5a18d7c9f32ffbb416d185cebd3a5c8aea0bfb6e
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2021-10-14 11:46:12 +0700

    test(component): add more test cases to the MesageContent

--- src/rogu/ui/MessageContent/__tests__/MessageContent.spec.js
+++ src/rogu/ui/MessageContent/__tests__/MessageContent.spec.js
@@ -1,9 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import MessageContent from "../index";
 
-import { BASIC_MESSAGE_A_1 } from "../../../../../__mocks__/messagesMock";
+import {
+  BASIC_MESSAGE_A_1,
+  USER_ID_A,
+} from "../../../../../__mocks__/messagesMock";
 
 // TODO: add more test cases
 describe("MessageContent", () => {
@@ -21,5 +25,82 @@ describe("MessageContent", () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should be rendered with the classname of `.rogu-message-content`", function () {
+    const component = shallow(
+      <MessageContent
+        userId={"random-user-id"}
+        channel={{
+          isGroupChannel: () => true,
+          getUnreadMemberCount: (_) => 10,
+          getUndeliveredMemberCount: (_) => 0,
+        }}
+        message={BASIC_MESSAGE_A_1}
+      />
+    );
+    expect(component.hasClass("rogu-message-content")).toBe(true);
+  });
+
+  it("should be rendered with the className of `.rogu-message-content--incoming` to outgoing message", function () {
+    const component = shallow(
+      <MessageContent
+        userId={"random-user-id"}
+        channel={{
+          isGroupChannel: () => true,
+          getUnreadMemberCount: (_) => 10,
+          getUndeliveredMemberCount: (_) => 0,
+        }}
+        message={BASIC_MESSAGE_A_1}
+      />
+    );
+    expect(component.hasClass("rogu-message-content--incoming")).toBe(true);
+  });
+
+  it("should be rendered with the className of `.rogu-message-content--outgoing` to incoming message", function () {
+    const component = shallow(
+      <MessageContent
+        userId={USER_ID_A}
+        channel={{
+          isGroupChannel: () => true,
+          getUnreadMemberCount: (_) => 10,
+          getUndeliveredMemberCount: (_) => 0,
+        }}
+        message={BASIC_MESSAGE_A_1}
+      />
+    );
+    expect(component.hasClass("rogu-message-content--outgoing")).toBe(true);
+  });
+
+  it("should be rendered with the className of `.rogu-message-content--chain-bottom` if chainBottom is true", function () {
+    const component = shallow(
+      <MessageContent
+        userId={"random-user-id"}
+        chainBottom={true}
+        channel={{
+          isGroupChannel: () => true,
+          getUnreadMemberCount: (_) => 10,
+          getUndeliveredMemberCount: (_) => 0,
+        }}
+        message={BASIC_MESSAGE_A_1}
+      />
+    );
+    expect(component.hasClass("rogu-message-content--chain-bottom")).toBe(true);
+  });
+
+  it("should be rendered with the className of `.rogu-message-content--chain-top` if chainTop is true", function () {
+    const component = shallow(
+      <MessageContent
+        userId={"random-user-id"}
+        chainTop={true}
+        channel={{
+          isGroupChannel: () => true,
+          getUnreadMemberCount: (_) => 10,
+          getUndeliveredMemberCount: (_) => 0,
+        }}
+        message={BASIC_MESSAGE_A_1}
+      />
+    );
+    expect(component.hasClass("rogu-message-content--chain-top")).toBe(true);
   });
 });
