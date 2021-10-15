d1c0ec096d91b756e36a750e68b044f7272f0c98
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2021-10-15 08:57:49 +0700

    feat(component): handle clamped message

--- src/rogu/ui/TextMessageItemBody/index.tsx
+++ src/rogu/ui/TextMessageItemBody/index.tsx
@@ -1,5 +1,7 @@
-import React, { ReactElement } from "react";
+import React, { useEffect, useRef, useState, ReactElement } from "react";
 import { UserMessage } from "sendbird";
+
+import TextButton from "../TextButton";
 import "./index.scss";
 
 import Label, { LabelTypography, LabelColors } from "../Label";
@@ -7,28 +9,66 @@ import { getClassName } from "../../../utils";
 
 interface Props {
   className?: string | Array<string>;
+  isByMe?: boolean;
   message: UserMessage;
 }
 
+type ClampType = "init" | "clamped" | "expanded";
+
 export default function TextMessageItemBody({
   className,
+  isByMe = false,
   message,
 }: Props): ReactElement {
+  const [clampState, setClampState] = useState<ClampType>("init");
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (
+      textRef.current &&
+      textRef.current.scrollHeight > textRef.current.clientHeight
+    ) {
+      setClampState("clamped");
+    }
+  }, [textRef.current]);
+
+  function handleExpand() {
+    setClampState("expanded");
+  }
+
   return (
-    <div className={getClassName([className, "rogu-text-message-item-body"])}>
-      {message?.message.split(/\r/).map((word, i) =>
-        word === "" ? (
-          <br key={i} />
-        ) : (
-          <Label
-            className="rogu-text-message-item-body__message"
-            color={LabelColors.ONBACKGROUND_1}
-            key={i}
-            type={LabelTypography.BODY_1}
-          >
-            {word}
-          </Label>
-        )
+    <div
+      className={getClassName([
+        className,
+        "rogu-text-message-item-body",
+        clampState == "expanded" ? "rogu-text-message-item-body--expanded" : "",
+        isByMe ? "rogu-text-message-item-body--outgoing" : "",
+      ])}
+    >
+      <div ref={textRef} className="rogu-text-message-item-body__inner">
+        {message?.message.split(/\r/).map((word, i) =>
+          word === "" ? (
+            <br key={i} />
+          ) : (
+            <Label
+              className="rogu-text-message-item-body__message"
+              color={LabelColors.ONBACKGROUND_1}
+              key={i}
+              type={LabelTypography.BODY_1}
+            >
+              {word}
+            </Label>
+          )
+        )}
+      </div>
+
+      {clampState === "clamped" && (
+        <TextButton
+          className="rogu-text-message-item-body__read-more"
+          onClick={handleExpand}
+        >
+          Read more
+        </TextButton>
       )}
     </div>
   );
