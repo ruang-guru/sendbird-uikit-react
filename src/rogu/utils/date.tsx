47786ff7823f2818910876712e01f4dc80f05cb3
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2021-10-21 13:00:23 +0700

    feat: add DateSeparator component

---
+++ src/rogu/utils/date.tsx
@@ -0,0 +1,6 @@
+import { Day } from "date-fns";
+
+export type DayStringType = { [key in Day]: string };
+
+export const getDayString = (dayNumber: Day, strings: DayStringType) =>
+  strings[dayNumber];
