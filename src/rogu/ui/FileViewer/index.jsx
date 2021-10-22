e4f8246cfa7f7876a35bf281d2f132b9f6c14d99
fauzan.linnas <fauzan.linnas@ruangguru.com>
2021-10-22 10:10:01 +0700

    feat(component): add media caption

--- src/rogu/ui/FileViewer/index.jsx
+++ src/rogu/ui/FileViewer/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { format } from 'date-fns';
@@ -8,17 +8,83 @@ import Avatar from '../Avatar/index';
 import Label, { LabelTypography, LabelColors } from '../Label';
 import Icon, { IconTypes } from '../Icon';
 import { MODAL_ROOT } from '../../../hooks/useModal/ModalRoot';
+import TextButton from '../TextButton';
 
-import { isImage, isVideo, isSupportedFileView } from '../../../utils';
+import { isImage, isVideo, isSupportedFileView, getClassName } from '../../../utils';
 import { LocalizationContext } from '../../../lib/LocalizationContext';
 import Toast from '../Toast';
+
+const ContentCaption = ({ captionMsg, isHidden }) => {
+  const { stringSet } = useContext(LocalizationContext);
+  const [clampState, setClampState] = useState('init');
+  const textRef = useRef();
+
+  useEffect(() => {
+    if (
+      textRef.current
+      && textRef.current.scrollHeight > textRef.current.clientHeight
+    ) {
+      setClampState('clamped');
+    }
+  }, [textRef.current]);
+
+  function handleExpand() {
+    setClampState('expanded');
+  }
+
+  return (
+    <div
+      className={getClassName([
+        'rogu-text-message-item-body',
+        clampState === 'expanded' ? 'rogu-text-message-item-body--expanded' : '',
+        isHidden ? 'hidden' : '',
+      ])}
+    >
+      <div ref={textRef} className="rogu-text-message-item-body__inner">
+        {captionMsg.split(/\r/).map((word, i) => (word === '' ? (
+          // eslint-disable-next-line react/no-array-index-key
+          <br key={i} />
+        ) : (
+          <Label
+            className="rogu-text-message-item-body__message"
+            color={LabelColors.ONBACKGROUND_1}
+            // eslint-disable-next-line react/no-array-index-key
+            key={i}
+            type={LabelTypography.BODY_1}
+          >
+            {word}
+          </Label>
+        )))}
+      </div>
+
+      {clampState === 'clamped' && (
+        <TextButton
+          className="rogu-text-message-item-body__read-more"
+          // eslint-disable-next-line react/jsx-no-bind
+          onClick={handleExpand}
+        >
+          <Label>{stringSet.BUTTON__READ_MORE}</Label>
+        </TextButton>
+      )}
+    </div>
+  );
+};
+
+ContentCaption.propTypes = {
+  captionMsg: PropTypes.string.isRequired,
+  isHidden: PropTypes.bool,
+};
+
+ContentCaption.defaultProps = {
+  isHidden: false,
+};
 
 export const FileViewerComponent = ({
   // sender
   profileUrl,
   userName,
   // file
-  fileName,
+  captionMsg,
   type,
   url,
   // others
@@ -30,6 +96,17 @@ export const FileViewerComponent = ({
   const { stringSet } = useContext(LocalizationContext);
 
   const [showToast, setShowToast] = useState(false);
+  const [isCaptionHidden, setIsCaptionHidden] = useState(false);
+  const contentRef = useRef();
+
+  const onMediaFocus = () => {
+    setIsCaptionHidden(true);
+  };
+
+  const onMediaBlur = () => {
+    setIsCaptionHidden(false);
+  };
+
   const onDownloadClick = () => {
     setShowToast(true);
     setTimeout(() => {
@@ -108,19 +185,31 @@ export const FileViewerComponent = ({
       <div className="rogu-fileviewer__content">
         {isVideo(type) && (
           // eslint-disable-next-line jsx-a11y/media-has-caption
-          <video controls className="rogu-fileviewer__content__video">
+          <video
+            onFocus={onMediaFocus}
+            onBlur={onMediaBlur}
+            controls
+            className="rogu-fileviewer__content__video"
+            ref={contentRef}
+          >
             <source src={url} type={type} />
           </video>
         )}
         {
           isImage(type) && (
             <img
+              onFocus={onMediaFocus}
+              onBlur={onMediaBlur}
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+              tabIndex="0"
+              ref={contentRef}
               src={url}
-              alt={fileName}
+              alt={`Uploaded by ${userName}`}
               className="rogu-fileviewer__content__img"
             />
           )
         }
+        {captionMsg && <ContentCaption captionMsg={captionMsg} isHidden={isCaptionHidden} />}
         {
           !isSupportedFileView(type) && (
             <div className="rogu-fileviewer__content__unsupported">
@@ -143,7 +232,7 @@ FileViewerComponent.propTypes = {
   userName: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  fileName: PropTypes.string.isRequired,
+  captionMsg: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
   isByMe: PropTypes.bool,
@@ -165,7 +254,7 @@ export default function FileViewer(props) {
     sender,
     type,
     url,
-    name: fileName = '',
+    name: captionMsg = '',
     createdAt = 0,
   } = message;
   const { profileUrl, nickname: userName = '' } = sender;
@@ -176,7 +265,7 @@ export default function FileViewer(props) {
         userName={userName}
         type={type}
         url={url}
-        fileName={fileName}
+        captionMsg={captionMsg}
         onClose={onClose}
         onDelete={onDelete}
         isByMe={isByMe}
--- src/rogu/ui/FileViewer/stories/FileViewer.stories.js
+++ src/rogu/ui/FileViewer/stories/FileViewer.stories.js
@@ -3,26 +3,34 @@ import FileViewer from '../index.jsx';
 
 export default { title: 'ruangkelas/UI Components/FileViewer' };
 
+import SendbirdProvider from "../../../../lib/Sendbird";
 import { msg1, msg0 } from '../data.mock';
+import COLOR_SET from "../../../../../__mocks__/themeMock";
 
 export const imageViewer = () => (
-  <FileViewer
-    onClose={() => { }}
-    onDelete={() => { }}
-    message={msg0}
-  />
+  <SendbirdProvider colorSet={COLOR_SET}>
+    <FileViewer
+      onClose={() => { }}
+      onDelete={() => { }}
+      message={msg0}
+    />
+  </SendbirdProvider>
 );
 export const movieViewer = () => (
-  <FileViewer
-    onClose={() => { }}
-    onDelete={() => { }}
-    message={msg1}
-  />
+  <SendbirdProvider colorSet={COLOR_SET}>
+    <FileViewer
+      onClose={() => { }}
+      onDelete={() => { }}
+      message={msg1}
+    />
+  </SendbirdProvider>
 );
 export const unSupportedViewer = () => (
-  <FileViewer
-    onClose={() => { }}
-    onDelete={() => { }}
-    message={{ sender: {} }}
-  />
+  <SendbirdProvider colorSet={COLOR_SET}>
+    <FileViewer
+      onClose={() => { }}
+      onDelete={() => { }}
+      message={{ sender: {} }}
+    />
+  </SendbirdProvider>
 );
