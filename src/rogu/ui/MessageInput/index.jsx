4aa6382448bd01aa1cb3a833aab3ec1ed7eff322
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2021-10-26 09:54:31 +0700

    feat: limit file size and format

--- src/rogu/ui/MessageInput/index.jsx
+++ src/rogu/ui/MessageInput/index.jsx
@@ -10,22 +10,17 @@ import { getClassName } from '../../../utils';
 
 import Icon, { IconTypes, IconColors } from '../Icon';
 import Label, { LabelTypography, LabelColors } from '../Label';
+import Toast from '../Toast';
 
 import './index.scss';
 
+const MAX_FILE_SIZE = 10000000; // 10MB;
+const TOAST_AUTO_HIDE_DURATION = 3000;
 const LINE_HEIGHT = 36;
 const noop = () => {};
 const KeyCode = {
   SHIFT: 16,
   ENTER: 13,
-};
-
-const handleUploadFile = (callback) => (event) => {
-  if (event.target.files && event.target.files[0]) {
-    callback(event.target.files[0]);
-  }
-  // eslint-disable-next-line no-param-reassign
-  event.target.value = '';
 };
 
 const MessageInput = React.forwardRef((props, ref) => {
@@ -46,6 +41,34 @@ const MessageInput = React.forwardRef((props, ref) => {
   const fileInputRef = useRef(null);
   const [inputValue, setInputValue] = useState(value);
   const [isShiftPressed, setIsShiftPressed] = useState(false);
+
+  // TODO: abstract the auto hide mechanism to the Toast component
+  const [showUploadErrorToast, setShowUploadErrorToast] = useState(false);
+  const autoHideTimer = useRef(null);
+  useEffect(() => {
+    if (showUploadErrorToast) {
+      clearTimeout(autoHideTimer.current);
+      autoHideTimer.current = setTimeout(() => {
+        setShowUploadErrorToast(false);
+      }, TOAST_AUTO_HIDE_DURATION);
+    }
+
+    return () => clearTimeout(autoHideTimer.current);
+  }, [showUploadErrorToast]);
+
+  const handleUploadFile = (callback) => (event) => {
+    const file = event.target?.files[0];
+
+    if (file) {
+      if (file.size > MAX_FILE_SIZE) {
+        setShowUploadErrorToast(true);
+      } else {
+        callback(file);
+      }
+    }
+    // eslint-disable-next-line no-param-reassign
+    event.target.value = '';
+  };
 
   const setHeight = () => {
     try {
@@ -88,56 +111,57 @@ const MessageInput = React.forwardRef((props, ref) => {
   };
 
   return (
-    <form
-      className={[
-        isEdit ? 'rogu-message-input__edit' : '',
-        disabled ? 'rogu-message-input-form__disabled ' : '',
-      ].join(' rogu-message-input__container ')}
-    >
-      <div
+    <>
+      <form
         className={[
-          'rogu-message-input',
-          disabled ? 'rogu-message-input__disabled' : '',
-        ].join(' ')}
+          isEdit ? 'rogu-message-input__edit' : '',
+          disabled ? 'rogu-message-input-form__disabled ' : '',
+        ].join(' rogu-message-input__container ')}
       >
-        <textarea
-          className="rogu-message-input--textarea"
-          disabled={disabled}
-          ref={ref}
-          name={name}
-          value={inputValue}
-          maxLength={maxLength}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-            onStartTyping();
-          }}
-          onKeyDown={(e) => {
-            if (e.keyCode === KeyCode.SHIFT) {
-              setIsShiftPressed(true);
-            }
-            if (!isShiftPressed && e.keyCode === KeyCode.ENTER) {
-              e.preventDefault();
-              sendMessage();
-            }
-          }}
-          onKeyUp={(e) => {
-            if (e.keyCode === KeyCode.SHIFT) {
-              setIsShiftPressed(false);
-            }
-          }}
-        />
-        {/* placeholder */}
-        {!inputValue && (
-          <Label
-            className="rogu-message-input--placeholder"
-            type={LabelTypography.BODY_1}
-            color={LabelColors.ONBACKGROUND_3}
-          >
-            {placeholder || stringSet.CHANNEL__MESSAGE_INPUT__PLACE_HOLDER}
-          </Label>
-        )}
-        {/* send icon */}
-        {/* {
+        <div
+          className={[
+            'rogu-message-input',
+            disabled ? 'rogu-message-input__disabled' : '',
+          ].join(' ')}
+        >
+          <textarea
+            className="rogu-message-input--textarea"
+            disabled={disabled}
+            ref={ref}
+            name={name}
+            value={inputValue}
+            maxLength={maxLength}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+              onStartTyping();
+            }}
+            onKeyDown={(e) => {
+              if (e.keyCode === KeyCode.SHIFT) {
+                setIsShiftPressed(true);
+              }
+              if (!isShiftPressed && e.keyCode === KeyCode.ENTER) {
+                e.preventDefault();
+                sendMessage();
+              }
+            }}
+            onKeyUp={(e) => {
+              if (e.keyCode === KeyCode.SHIFT) {
+                setIsShiftPressed(false);
+              }
+            }}
+          />
+          {/* placeholder */}
+          {!inputValue && (
+            <Label
+              className="rogu-message-input--placeholder"
+              type={LabelTypography.BODY_1}
+              color={LabelColors.ONBACKGROUND_3}
+            >
+              {placeholder || stringSet.CHANNEL__MESSAGE_INPUT__PLACE_HOLDER}
+            </Label>
+          )}
+          {/* send icon */}
+          {/* {
           (!isEdit && inputValue && inputValue.trim().length > 0) && (
             <IconButton
               className="rogu-message-input--send"
@@ -154,78 +178,87 @@ const MessageInput = React.forwardRef((props, ref) => {
             </IconButton>
           )
         } */}
-        {/* upload icon */}
-        {!isEdit && (
-          <IconButton
-            className="rogu-message-input--attach"
-            height="32px"
-            width="32px"
-            onClick={() => {
-              // todo: clear previous input
-              fileInputRef.current.click();
-            }}
-          >
-            <Icon
-              type={IconTypes.ATTACH}
-              fillColor={IconColors.CONTENT_INVERSE}
-              width="20px"
-              height="20px"
-            />
-            <input
-              className="rogu-message-input--attach-input"
-              type="file"
-              ref={fileInputRef}
-              onChange={handleUploadFile(onFileUpload)}
-            />
-          </IconButton>
-        )}
+          {/* upload icon */}
+          {!isEdit && (
+            <IconButton
+              className="rogu-message-input--attach"
+              height="32px"
+              width="32px"
+              onClick={() => {
+                // todo: clear previous input
+                fileInputRef.current.click();
+              }}
+            >
+              <Icon
+                type={IconTypes.ATTACH}
+                fillColor={IconColors.CONTENT_INVERSE}
+                width="20px"
+                height="20px"
+              />
+              <input
+                accept=".doc,.docx,.xls,.xlsx,.ppt,.pptx,.pdf,image/*,.mov,.mp4"
+                className="rogu-message-input--attach-input"
+                type="file"
+                ref={fileInputRef}
+                onChange={handleUploadFile(onFileUpload)}
+              />
+            </IconButton>
+          )}
 
-        {!isEdit && (
-          <IconButton
-            className={getClassName(['rogu-message-input--send', disabled ? 'rogu-message-input--send-disabled' : ''])}
-            height="36px"
-            width="36px"
-            onClick={sendMessage}
-          >
-            <Icon
-              type={IconTypes.SEND}
-              fillColor={IconColors.WHITE}
-              width="16px"
-              height="16px"
-            />
-          </IconButton>
-        )}
-      </div>
-      {/* Edit */}
-
-      {isEdit && (
-        <div className="rogu-message-input--edit-action">
-          <Button
-            className="rogu-message-input--edit-action__cancel"
-            type={ButtonTypes.SECONDARY}
-            size={ButtonSizes.SMALL}
-            onClick={onCancelEdit}
-          >
-            {stringSet.BUTTON__CANCEL}
-          </Button>
-          <Button
-            className="rogu-message-input--edit-action__save"
-            type={ButtonTypes.PRIMARY}
-            size={ButtonSizes.SMALL}
-            onClick={() => {
-              if (inputValue) {
-                const trimmedInputValue = inputValue.trim();
-                onSendMessage(name, trimmedInputValue, () => {
-                  onCancelEdit();
-                });
-              }
-            }}
-          >
-            {stringSet.BUTTON__SAVE}
-          </Button>
+          {!isEdit && (
+            <IconButton
+              className={getClassName([
+                'rogu-message-input--send',
+                disabled ? 'rogu-message-input--send-disabled' : '',
+              ])}
+              height="36px"
+              width="36px"
+              onClick={sendMessage}
+            >
+              <Icon
+                type={IconTypes.SEND}
+                fillColor={IconColors.WHITE}
+                width="16px"
+                height="16px"
+              />
+            </IconButton>
+          )}
         </div>
+        {/* Edit */}
+
+        {isEdit && (
+          <div className="rogu-message-input--edit-action">
+            <Button
+              className="rogu-message-input--edit-action__cancel"
+              type={ButtonTypes.SECONDARY}
+              size={ButtonSizes.SMALL}
+              onClick={onCancelEdit}
+            >
+              {stringSet.BUTTON__CANCEL}
+            </Button>
+            <Button
+              className="rogu-message-input--edit-action__save"
+              type={ButtonTypes.PRIMARY}
+              size={ButtonSizes.SMALL}
+              onClick={() => {
+                if (inputValue) {
+                  const trimmedInputValue = inputValue.trim();
+                  onSendMessage(name, trimmedInputValue, () => {
+                    onCancelEdit();
+                  });
+                }
+              }}
+            >
+              {stringSet.BUTTON__SAVE}
+            </Button>
+          </div>
+        )}
+      </form>
+
+      {showUploadErrorToast && (
+        <Toast message={stringSet.TOAST__MAX_FILE_SIZE_ERROR} />
       )}
-    </form>
+    </>
   );
 });
 
