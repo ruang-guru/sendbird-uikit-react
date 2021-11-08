9ad3e1bdd97402d79f13ed27826bbcb3ed261381
fauzan.linnas <fauzan.linnas@ruangguru.com>
2021-11-08 15:31:08 +0700

    fix: hide arrow in chained bubble message

--- src/rogu/ui/MessageContent/index.tsx
+++ src/rogu/ui/MessageContent/index.tsx
@@ -82,8 +82,8 @@ export default function MessageContent({
   resendMessage,
   disabled = false,
 }: // showRemove,
-// toggleReaction,
-Props): ReactElement {
+  // toggleReaction,
+  Props): ReactElement {
   const { stringSet } = useContext(LocalizationContext);
   const messageTypes = getUIKitMessageTypes();
   const avatarRef = useRef(null);
@@ -212,11 +212,11 @@ Props): ReactElement {
               )}
               {getUIKitMessageType(message as FileMessage) ===
                 messageTypes.FILE && (
-                <FileMessageItemBody
-                  message={message as FileMessage}
-                  isByMe={isByMe}
-                />
-              )}
+                  <FileMessageItemBody
+                    message={message as FileMessage}
+                    isByMe={isByMe}
+                  />
+                )}
               {isThumbnailMessage(message as FileMessage) && (
                 <>
                   <ThumbnailMessageItemBody
@@ -239,10 +239,10 @@ Props): ReactElement {
               )}
               {getUIKitMessageType(message as FileMessage) ===
                 messageTypes.UNKNOWN && (
-                <UnknownMessageItemBody message={message} isByMe={isByMe} />
-              )}
+                  <UnknownMessageItemBody message={message} isByMe={isByMe} />
+                )}
             </div>
-            {((!isByMe && chainTop) || isByMe) && (
+            {(((!isByMe && chainTop) || isByMe) && !channel.isFrozen) && (
               <MessageItemMenu
                 className="rogu-message-content__menu"
                 channel={channel}
