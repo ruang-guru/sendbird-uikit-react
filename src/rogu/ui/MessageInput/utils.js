7eaf3a4816abe02363ca67a4c6741b12b50adde4
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2021-11-16 09:30:39 +0700

    fix: add https:// to a url that passed to LinkPreview

--- src/rogu/ui/MessageInput/utils.js
+++ src/rogu/ui/MessageInput/utils.js
@@ -3,8 +3,6 @@
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 
-import { isUrl } from '../../../utils';
-
 // leading edge, instead of the trailing.
 export function debounce(func, wait, immediate) {
   let timeout;
@@ -21,13 +19,6 @@ export function debounce(func, wait, immediate) {
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
-}
-
-export function getUrlFromWords(inputValue, setUrl) {
-  const inputValueArray = inputValue.split(/\s+/);
-  const url = inputValueArray.find((word) => isUrl(word));
-  const hasUrl = !!url;
-  return hasUrl && setUrl({ hasUrl: true, text: url });
 }
 
 export default debounce;
