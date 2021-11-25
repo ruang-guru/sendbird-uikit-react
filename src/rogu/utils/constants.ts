7f56e543eff9f430f0df0a8531189b06ea431134
fauzan.linnas <fauzan.linnas@ruangguru.com>
2021-11-25 15:03:11 +0700

    Add notes, source of regex

--- src/rogu/utils/constants.ts
+++ src/rogu/utils/constants.ts
@@ -3,4 +3,7 @@ export const REPLIED_MESSAGE_MAX_CHAR = 200;
 export const REPLIED_MESSAGE_QUOTE_FORMAT = '>';
 
 export const REGEX_LINE_BREAK = /\r?\n|\r/g;
+
+// Note: Source thread: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+// Note: Source demo: https://regexr.com/3e6m0
 export const REGEX_URL = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
