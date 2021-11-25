4781494c7ad8a0e03be393e2432b1d7786ac6683
fauzan.linnas <fauzan.linnas@ruangguru.com>
2021-11-25 14:43:59 +0700

    fix: url regex

--- src/rogu/utils/constants.ts
+++ src/rogu/utils/constants.ts
@@ -3,4 +3,4 @@ export const REPLIED_MESSAGE_MAX_CHAR = 200;
 export const REPLIED_MESSAGE_QUOTE_FORMAT = '>';
 
 export const REGEX_LINE_BREAK = /\r?\n|\r/g;
-export const REGEX_URL = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*?)/g;
+export const REGEX_URL = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
